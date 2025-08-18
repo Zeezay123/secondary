78a517fc9b90a6408556ae90bc68603d8cb9badb
Zeezay <aolam.ao@gmail.com>
2025-08-18 10:19:34 +0100

    postgrad

--- postgrad-page/src/firebase.js
+++ postgrad-page/src/firebase.js
@@ -3,17 +3,16 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "codeldatabase.firebaseapp.com",
-  projectId: "codeldatabase",
-  storageBucket: "codeldatabase.firebasestorage.app",
-  messagingSenderId: "97717270524",
-  appId: "1:97717270524:web:dc9ff3e6b9179afc9ba479"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
+  authDomain: "postgrad-41f47.firebaseapp.com",
+  projectId: "postgrad-41f47",
+  storageBucket: "postgrad-41f47.firebasestorage.app",
+  messagingSenderId: "233784182578",
+  appId: "1:233784182578:web:40980fa8ab991ef920392b"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
