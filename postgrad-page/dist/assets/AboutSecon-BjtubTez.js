627fec25f60460573816376a7a03dea6ea8f4541
Zeezay123 <aolam.ao@gmail.com>
2026-01-26 15:04:19 +0100

    dist upload for plesk

---
+++ postgrad-page/dist/assets/AboutSecon-BjtubTez.js
@@ -0,0 +1,11 @@
+import{j as e}from"./index-B-2_T1Uz.js";const t="/assets/school4-D3iptg0J.jpg",a=()=>{const o={content:`Welcome to the website of University of Lagos Staff School Akoka Lagos.
+     The School, located within the Akoka Campus was established on the 10th of October, 1966. 
+     Being a community school, it was initially founded with the objective of providing primary 
+     school education for the children of staff of the University. The Annex of the school at Idi-Araba
+      was founded on 18th January, 1983 and the objective of its establishment like the main School was 
+      to cater for primary education needs of children of staff in College of Medicine (CMUL) and Lagos
+       University Teaching Hospital (LUTH). With expansion in the facilities of the school and the need to 
+       be socially responsible, children of non-staff are given the privilege of admission opportunity. In 
+       both the main School and the Annex, there are Nursery sections, that are well equipped and manned by
+        qualified and experienced professionals trained in early childhood education.`,name:"Mr. Agohogoh",post:"Principal"};return e.jsx("main",{className:"flex flex-col items-center m-2 md:m-5",children:e.jsxs("section",{className:` max-w-full h-[80vh] md:max-h-[70vh]\r
+    rounded-3xl  flex flex-col p-1 md:px-5 md:mx-3  md:flex-row md:mt-25 `,children:[e.jsxs("div",{className:"flex flex-col w-full md:w-50% p-2 md:p-15",children:[e.jsx("p",{className:"w-full md:w-[90%] mt-5 font-[inter] text-sm md:text-lg/relaxed text-justify",children:o.content}),e.jsxs("p",{className:"mt-5 font-[inter] text-lg font-bold text-black",children:[o.name," "]}),e.jsx("p",{className:"text-sm mb-2 font-[inter] font-normal",children:o.post})]}),e.jsx("div",{className:"w-45% hidden md:flex ",children:e.jsx("div",{className:"w-[500px] h-[500px] object-cover rounded-3xl shadow",children:e.jsx("img",{src:t,alt:"",className:"w-full h-full rounded-3xl"})})})]})})};export{a as default};
